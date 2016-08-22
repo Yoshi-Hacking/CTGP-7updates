2d73c45d4a704ec4b8a9296474478c5aeeba28b4
mariohackandglitch <hackyglitch@gmail.com>
2016-08-22 18:52:20 +0200

    Update uinfo.lua

--- uinfo.lua
+++ uinfo.lua
@@ -2,14 +2,17 @@ function updateinfo(updinf)
 	local ver = ""
 	local changelog = {}
 	if (updinf == nil) then
-		return 2
+		return 3
 	elseif (updinf == 0) then
+		ver = "0.8.2"
+		changelog = {"+ Fixed 0.8 bug with N3DS","","+ Updated Miku's Birthday Spec."}
+	elseif (updinf == 1) then
 		ver = "0.8.1"
 		changelog = {"+ Update needed to make 0.8 work properly."}
-	elseif (updinf == 1) then
+	elseif (updinf == 2) then
 		ver = "0.8"
 		changelog = {"+ Added GBA Cheese Land", "+ Added Galvarny Falls", "+ Added GCN Wario Colosseum", "", "+ Updated N64 Rainbow Road", "+ Updated GBA Bowser Castle 3", "+ Updated Miku's Birthday Spec."}
-	elseif (updinf == 2) then
+	elseif (updinf == 3) then
 		ver = "0.7.2"
 		changelog = {"+ Fixed the downloader"}
 	end

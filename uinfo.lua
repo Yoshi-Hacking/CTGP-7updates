2b4cf57552f5a061dbd65bb89ee5b2d818260c33
PabloMK7 <hackyglitch@gmail.com>
2016-08-29 19:18:50 +0200

    Update uinfo.lua

--- uinfo.lua
+++ uinfo.lua
@@ -2,23 +2,26 @@ function updateinfo(updinf)
 	local ver = ""
 	local changelog = {}
 	if (updinf == nil) then
-		return 5
+		return 6
 	elseif (updinf == 0) then
 		ver = "0.8.4"
 		changelog = {"+ Fixed some tracks online."}
 	elseif (updinf == 1) then
+		ver = "0.8.4"
+		changelog = {"+ Fixed some tracks online."}
+	elseif (updinf == 2) then
 		ver = "0.8.3"
 		changelog = {"+ More bug fixes."}
-	elseif (updinf == 2) then
+	elseif (updinf == 3) then
 		ver = "0.8.2"
 		changelog = {"+ Fixed a bug with N3DS","","+ Updated Miku's Birthday Spec."}
-	elseif (updinf == 3) then
+	elseif (updinf == 4) then
 		ver = "0.8.1"
 		changelog = {"+ Update needed to make 0.8 work properly."}
-	elseif (updinf == 4) then
+	elseif (updinf == 5) then
 		ver = "0.8"
 		changelog = {"+ Added GBA Cheese Land", "+ Added Galvarny Falls", "+ Added GCN Wario Colosseum", "", "+ Updated N64 Rainbow Road", "+ Updated GBA Bowser Castle 3", "+ Updated Miku's Birthday Spec."}
-	elseif (updinf == 5) then
+	elseif (updinf == 6) then
 		ver = "0.7.2"
 		changelog = {"+ Fixed the downloader"}
 	end

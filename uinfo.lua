063e1925084bbe4a216440aee142aad808914e43
PabloMK7 <hackyglitch@gmail.com>
2016-09-02 17:36:12 +0200

    Update uinfo.lua

--- uinfo.lua
+++ uinfo.lua
@@ -2,26 +2,29 @@ function updateinfo(updinf)
 	local ver = ""
 	local changelog = {}
 	if (updinf == nil) then
-		return 6
+		return 7
 	elseif (updinf == 0) then
+		ver = "0.9"
+		changelog = {"+ Added Elemental cave", "+ Added Banshee Broadwalk","","+ Updated Miku's Birthday Spec."}
+	elseif (updinf == 1) then
 		ver = "0.8.5"
 		changelog = {"+ Fixed manager bugs", "+ Improved the updater", "","(New BootNTR version,","use BootNTR updater to update it.)"}
-	elseif (updinf == 1) then
+	elseif (updinf == 2) then
 		ver = "0.8.4"
 		changelog = {"+ Fixed some tracks online."}
-	elseif (updinf == 2) then
+	elseif (updinf == 3) then
 		ver = "0.8.3"
 		changelog = {"+ More bug fixes."}
-	elseif (updinf == 3) then
+	elseif (updinf == 4) then
 		ver = "0.8.2"
 		changelog = {"+ Fixed a bug with N3DS","","+ Updated Miku's Birthday Spec."}
-	elseif (updinf == 4) then
+	elseif (updinf == 5) then
 		ver = "0.8.1"
 		changelog = {"+ Update needed to make 0.8 work properly."}
-	elseif (updinf == 5) then
+	elseif (updinf == 6) then
 		ver = "0.8"
 		changelog = {"+ Added GBA Cheese Land", "+ Added Galvarny Falls", "+ Added GCN Wario Colosseum", "", "+ Updated N64 Rainbow Road", "+ Updated GBA Bowser Castle 3", "+ Updated Miku's Birthday Spec."}
-	elseif (updinf == 6) then
+	elseif (updinf == 7) then
 		ver = "0.7.2"
 		changelog = {"+ Fixed the downloader"}
 	end

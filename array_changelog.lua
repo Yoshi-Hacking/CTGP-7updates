eef822db0f319f1061881e452877fcdd88912450
PabloMK7 <hackyglitch@gmail.com>
2016-10-08 17:10:41 +0200

    Update array_changelog.lua

--- array_changelog.lua
+++ array_changelog.lua
@@ -1,2 +1,14 @@
 
-array_changelog = {{"+ Fixed Banshee Broadwalk 2 lag", "", "+ Added LED tricks plugin", "  (enable it from ingame settings)", "", "+ Improved the manager"}, {"+ Added custom music to Concord Town."}, {"+ Added Elemental Cave", "+ Added Banshee Broadwalk 2","","+ Updated Miku's Birthday Spec."}, {"+ Fixed manager bugs", "+ Improved the updater", "","(New BootNTR version,","use BootNTR updater to update it.)"}, {"+ Fixed some tracks online."}, {"+ More bug fixes."}, {"+ Fixed a bug with N3DS","","+ Updated Miku's Birthday Spec."}, {"+ Update needed to make 0.8 work properly."}, {"+ Added GBA Cheese Land", "+ Added Galvarny Falls", "+ Added GCN Wario Colosseum", "", "+ Updated N64 Rainbow Road", "+ Updated GBA Bowser Castle 3", "+ Updated Miku's Birthday Spec."}, {"+ Fixed the downloader"}}
+array_changelog = {
+	{"+ Fixed GBA Cheese Land bug.", "+ Fixed SNES Ghost Valley 2 bug.", "","+ Implemented Nanquita's BootNTR","  into the manager."},
+	{"+ Fixed Banshee Broadwalk 2 lag.", "", "+ Added LED tricks plugin", "  (enable it from ingame settings).", "", "+ Improved the manager."},
+	{"+ Added custom music to Concord Town."},
+	{"+ Added Elemental Cave.", "+ Added Banshee Broadwalk 2.","","+ Updated Miku's Birthday Spec."},
+	{"+ Fixed manager bugs.", "+ Improved the updater.", "","(New BootNTR version,","use BootNTR updater to update it.)"},
+	{"+ Fixed some tracks online."},
+	{"+ More bug fixes."},
+	{"+ Fixed a bug with N3DS","","+ Updated Miku's Birthday Spec."},
+	{"+ Update needed to make 0.8 work properly."},
+	{"+ Added GBA Cheese Land.", "+ Added Galvarny Falls.", "+ Added GCN Wario Colosseum.", "", "+ Updated N64 Rainbow Road.", "+ Updated GBA Bowser Castle 3.", "+ Updated Miku's Birthday Spec."},
+	{"+ Fixed the downloader."}
+}

ff8e7af0609abf68e9c56fe8a18b7595ff6e7aa6
PabloMK7 <hackyglitch@gmail.com>
2017-03-24 22:14:33 +0100

    Update array_changelog.lua

--- array_changelog.lua
+++ array_changelog.lua
@@ -1,5 +1,6 @@
 
 array_changelog = {
+	{"+ Updated OnionFS to latest version."},
 	{"+ Fixed menu edits to JPN game."},
 	{"+ Fixed lag caused by v0.12.3."},
 	{"+ Made the 1.1 update patch", "  compatible with CTGP-7.", "", "+ Added JPN MK7 text edits", "  (English only for now)."},
